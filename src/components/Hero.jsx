a8aaadc79f7d0bc10001cd9b9e27c9301052c712
mritunjay226 <113658885+mritunjay226@users.noreply.github.com>
2025-07-06 14:51:18 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero({ dark }) {
       }}
     >
       {/* Overlay */}
-{/*       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 z-10" /> */}
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 z-10" />
 
       {/* Background Image */}
       <img
