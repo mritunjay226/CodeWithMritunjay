858e12fb50acd22cc6e91d6da1ddc5c95bee2291
mritunjay mishra <mishramritunjay45com>
2025-07-11 22:49:38 +0530

    Resolve vite.config.js merge conflict

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,29 +40,5 @@ export default function Hero({ dark }) {
   </div>
 </section>
 
-<<<<<<< HEAD
-      {/* Background Image */}
-      <img
-        src="/mritunjay5.png"
-        alt="Mritunjay"
-        className="absolute top-0 left-0 w-full h-full object-cover object-center z-0 md:[40px] md:static md:w-1/2 md:h-full md:z-10"
-      />
-
-      {/* Text */}
-      <div className="z-10 max-w-xl py-12 text-center md:text-left text-white md:text-black dark:text-white">
-        <h1 className="text-4xl sm:text-5xl font-bold mb-4">Hi, I'm Mritunjay</h1>
-        <p className="mb-6 text-lg text-gray-200 md:text-gray-700 dark:text-gray-300">
-          I build fast, responsive eCommerce and custom websites using React, Next.js, Tailwind, and more.
-        </p>
-        <a
-          href="#contact"
-          className="inline-block font-semibold bg-white dark:bg-white dark:text-black text-black px-6 py-3 border-2 border-white hover:scale-[1.05] rounded-full  transition"
-        >
-          Let's Work Together
-        </a>
-      </div>
-    </section>
-=======
->>>>>>> 9c178e0 (hello)
   );
 }
